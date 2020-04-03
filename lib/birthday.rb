5fedf90a6202c3d2b5b0b2c0ca17cba2f1cc05e8
DAZUN XING <dazun.xing@gmail.com>
2020-04-03 05:19:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,15 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  
+  hash.each do |x,y|
+    puts "Happy Birthday #{x}! You are now #{y} years old!"
+  end
 end
+
 
 
