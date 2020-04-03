3ec2e688924e3445baf666e80f9d16eb1ccdf3f0
DAZUN XING <dazun.xing@gmail.com>
2020-04-03 05:24:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age<=12
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
   end
 end
